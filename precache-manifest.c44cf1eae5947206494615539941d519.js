158cb7fa3f8d82382c1d74c448e513b160ec19b9
Shiva804 <krishnansai49@gmail.com>
2020-06-29 12:51:53 +0530

    Updates

---
+++ precache-manifest.c44cf1eae5947206494615539941d519.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f7bcd20cb7615f8ae67ccf142c8a0d04",
+    "url": "/notes_keeper/index.html"
+  },
+  {
+    "revision": "074fdc816f7b62c6137b",
+    "url": "/notes_keeper/static/css/main.b9c912e0.chunk.css"
+  },
+  {
+    "revision": "86910c078687cdac4794",
+    "url": "/notes_keeper/static/js/2.d9e4917d.chunk.js"
+  },
+  {
+    "revision": "0135287500bd9d64203d07b4f33f971f",
+    "url": "/notes_keeper/static/js/2.d9e4917d.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "074fdc816f7b62c6137b",
+    "url": "/notes_keeper/static/js/main.78731cad.chunk.js"
+  },
+  {
+    "revision": "d87584f0f00d44eb1053",
+    "url": "/notes_keeper/static/js/runtime-main.1008204e.js"
+  }
+]);
